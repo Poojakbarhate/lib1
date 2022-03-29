0ab3c94763f90d68f4d28505affeb142466b61ed
poojakbarhate <poojakbarhate6598@gmail.com>
2022-03-29 18:57:28 +0530

    Initial Commit

--- app/src/main/java/com/example/librarycreationwith5/MainActivity.java
+++ app/src/main/java/com/example/librarycreationwith5/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toasty.sundaykoaana(this,"sunday ko mast naha dho ke aa");
-    }
+        Toasty.printMyToast(this,"Welcome to loylty rewardz");
+       }
 }
